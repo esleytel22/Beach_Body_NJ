74cfccb3d6f8ee3476763b58873c44bf342b40d4
Cristian Bonifacio <Cristian.Bonifacio23@qmail.cuny.edu>
2026-03-30 20:04:42 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/',
+  base: command === 'serve' ? '/' : '/Beach_Body_NJ/',
 }))
